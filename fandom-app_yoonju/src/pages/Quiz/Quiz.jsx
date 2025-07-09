6c5e4d0f4aacbaf6ac884586803e52e0213db7a9
JungYoonju <sksmswjddbsw50@gmail.com>
2025-07-09 16:54:19 +0900

    DM 수정 완료

---
+++ fandom-app_yoonju/src/pages/Quiz/Quiz.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import BackButton from '../../components/Backbutton'
+
+const Quiz = () => {
+  return (
+    <div>
+    <BackButton/>
+    <h2>퀴즈 페이지</h2>
+    </div>
+  )
+}
+
+export default Quiz
