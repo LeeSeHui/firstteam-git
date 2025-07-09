b718533393d52f572c2cf764500fd7c4b687e4a4
jongminbag92 <jongminbag92@gmail.com>
2025-07-09 10:38:45 +0900

    Add new Home page components

---
+++ src/pages/Vote/Vote.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import BackButton from '../../components/Backbutton'
+
+const Vote = () => {
+  return (
+    <div>
+      <BackButton/>
+      <h2>투표 페이지</h2>
+    </div>
+  )
+}
+
+export default Vote
