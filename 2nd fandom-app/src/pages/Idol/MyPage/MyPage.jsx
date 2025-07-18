c848585936c6754182e49e30203eb4c52ce45305
kimhyoju <rlagywn8804@naver.com>
2025-07-18 17:47:27 +0900

    � 로컬 변경사항 저장 커밋

--- 2nd fandom-app/src/pages/Idol/MyPage/MyPage.jsx
+++ 2nd fandom-app/src/pages/Idol/MyPage/MyPage.jsx
@@ -6,8 +6,12 @@ import useNickname from '../../../contexts/useNickname';
 import starlogo from '../../../assets/mypage/starlogo.png';
 import video1 from '../../../assets/mypage/video1.png';
 import levelbar from '../../../assets/mypage/levelbar.png';
-
 import './MyPage.css'; // ✅ 스타일 import
+
+
+
+
+
 
 const MyPage = () => {
   const navigate = useNavigate();
@@ -24,7 +28,7 @@ const MyPage = () => {
       </h2>
 
       <p>
-        <img src={starlogo} alt="star" />
+        <img className='star-logo' src={starlogo} alt="star" />
         <span>보유 포인트 : 180</span>
       </p>
 
@@ -37,7 +41,7 @@ const MyPage = () => {
         <p>
           {nickname}님, 오늘의 챌린지를 통해<br />목표에 달성해보아요!
         </p>
-        <span>별별이 LV.1</span>
+        <span>별별이 L.1</span>
         <img src={levelbar} alt="level bar" />
         <span>조금만 더 가면 Lv.2에 달성해요</span>
       </div>
@@ -46,8 +50,24 @@ const MyPage = () => {
         <button onClick={() => navigate('/idol/mypage/collection')}>나의 컬렉션</button>
         <button onClick={() => navigate('/idol/mypage/payment')}>결제내역(멤버십관리)</button>
         <button onClick={() => navigate('/idol/mypage/media')}>최근 본 미디어</button>
-        <button onClick={toggleTheme}>
-          {darkMode ? '☀️ 라이트 모드' : '🌙 다크 모드'}
+       
+       {/* 다크모드 토글 */}
+        <button className="setting-button">
+        <span className="setting-label">어플설정(다크모드 알림 설정)</span>
+        <div className="toggle-switch">
+            <span
+              className={`toggle-option ${darkMode ? 'active' : ''}`}
+              onClick={() => !darkMode && toggleTheme()}  // 다크모드 켜기
+            >
+              on
+            </span>
+            <span
+              className={`toggle-option ${!darkMode ? 'active' : ''}`}
+              onClick={() => darkMode && toggleTheme()}   // 다크모드 끄기
+            >
+              off
+            </span>
+          </div>
         </button>
       </div>
 
