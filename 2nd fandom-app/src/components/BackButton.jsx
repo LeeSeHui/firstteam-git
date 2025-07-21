f5bd2a2241bcb293e15f53045ab160224d8f737a
kimhyoju <rlagywn8804@naver.com>
2025-07-21 18:06:25 +0900

    마이페이지 최근 본 미디어 헤더에 편집 텍스트 추가 및 정렬 수정

--- 2nd fandom-app/src/components/BackButton.jsx
+++ 2nd fandom-app/src/components/BackButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Backbutton.css';
 
-const BackButton = ({ to, label = '뒤로가기', onPlusClick }) => {
+const BackButton = ({ to, label = '뒤로가기', onPlusClick, editText, onEditClick }) => {
   const navigate = useNavigate();
 
   const handleBack = () => {
@@ -16,10 +16,21 @@ const BackButton = ({ to, label = '뒤로가기', onPlusClick }) => {
   return (
     <div className="ButtonContainer">
       <button className="back-arrow" onClick={handleBack}>←</button>
+
+      {/* 마이페이지- 컬렉션 + */}
       <span className="header-title">{label}</span>
       {onPlusClick && (
         <span className="plus-icon" onClick={onPlusClick}>+</span>
       )}
+
+       {/* 마이페이지- 최근 본 미디어 '편집' */}
+      {editText && onEditClick && (
+        <span className="right-action" onClick={onEditClick}>
+          {editText}
+        </span>
+      )}
+
+    
     </div>
   );
 };
